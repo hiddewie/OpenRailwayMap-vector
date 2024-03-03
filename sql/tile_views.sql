a128c90bbcb7b69e3b140a0e4b7cadc82afa74f2
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-03-03 12:17:11 +0100

    simplify duplicated tables

--- sql/tile_views.sql
+++ sql/tile_views.sql
@@ -1245,88 +1245,6 @@ CREATE OR REPLACE VIEW signals_railway_signals AS
 
 --- Electrification ---
 
-CREATE OR REPLACE VIEW electrification_railway_line_casing AS
-  SELECT
-     way, railway, usage, service,
-    CASE
-      WHEN railway = 'construction' THEN construction_railway
-      ELSE railway
-    END as feature,
-     construction,
-     construction_railway,
-     CASE WHEN railway = 'rail' AND usage IN ('tourism', 'military', 'test') AND service IS NULL THEN 400
-          WHEN railway = 'rail' AND usage IS NULL AND service IS NULL THEN 400
-          WHEN railway = 'rail' AND usage IS NULL AND service = 'siding' THEN 870
-          WHEN railway = 'rail' AND usage IS NULL AND service = 'yard' THEN 860
-          WHEN railway = 'rail' AND usage IS NULL AND service = 'spur' THEN 880
-          WHEN railway = 'rail' AND usage IS NULL AND service = 'crossover' THEN 300
-          WHEN railway = 'rail' AND usage = 'main' AND service IS NULL THEN 1100
-          WHEN railway = 'rail' AND usage = 'branch' AND service IS NULL THEN 1000
-          WHEN railway = 'rail' AND usage = 'industrial' AND service IS NULL THEN 850
-          WHEN railway = 'rail' AND usage = 'industrial' AND service IN ('siding', 'spur', 'yard', 'crossover') THEN 850
-          WHEN railway IN ('preserved', 'construction') THEN 400
-          ELSE 50
-       END AS rank,
-     electrified,
-     frequency,
-     voltage,
-     construction_electrified,
-     construction_frequency,
-     proposed_electrified,
-     proposed_frequency,
-     railway_no_to_null(deelectrified)
-  FROM
-    (SELECT
-       way, railway, usage, service,
-       construction,
-       tags->'construction:railway' AS construction_railway,
-       electrified,
-       frequency,
-       voltage,
-       construction_electrified,
-       construction_frequency,
-       proposed_electrified,
-       proposed_frequency,
-       deelectrified,
-       layer
-     FROM openrailwaymap_osm_line
-     WHERE railway IN ('rail', 'tram', 'light_rail', 'subway', 'narrow_gauge', 'construction')
-    ) AS r
-  ORDER by layer, rank NULLS LAST;
-
-CREATE OR REPLACE VIEW electrification_railway_line_low AS
-  SELECT
-    way, railway, usage,
-    railway as feature,
-    NULL AS service,
-    NULL AS construction,
-    NULL AS construction_railway,
-    NULL AS construction_usage, NULL AS construction_service,
-    NULL AS preserved_railway, NULL AS preserved_service,
-    NULL AS preserved_usage,
-    electrification_state AS state,
-    electrification_state_without_future AS state_now,
-    railway_voltage_for_state(electrification_state, voltage, construction_voltage, proposed_voltage) AS merged_voltage,
-    railway_frequency_for_state(electrification_state, frequency, construction_frequency, proposed_frequency) AS merged_frequency,
-    railway_to_int(voltage) AS voltage,
-    railway_to_float(frequency) AS frequency
-  FROM
-    (SELECT
-       way, railway, usage,
-       railway_electrification_state(railway, electrified, deelectrified, abandoned_electrified, construction_electrified, proposed_electrified, FALSE) AS electrification_state,
-       railway_electrification_state(railway, electrified, deelectrified, abandoned_electrified, NULL, NULL, TRUE) AS electrification_state_without_future,
-       frequency AS frequency,
-       voltage AS voltage,
-       construction_frequency AS construction_frequency,
-       construction_voltage AS construction_voltage,
-       proposed_frequency AS proposed_frequency,
-       proposed_voltage AS proposed_voltage,
-       layer
-     FROM openrailwaymap_osm_line
-     WHERE railway = 'rail' AND usage = 'main' AND service IS NULL
-    ) AS r
-  ORDER BY layer NULLS LAST;
-
 CREATE OR REPLACE VIEW electrification_railway_line_med AS
   SELECT
     way, railway, usage,
@@ -1366,7 +1284,7 @@ CREATE OR REPLACE VIEW electrification_railway_line_med AS
     layer,
     rank NULLS LAST;
 
-CREATE OR REPLACE VIEW electrification_railway_line_fill AS
+CREATE OR REPLACE VIEW electrification_railway_line AS
   SELECT
     way, railway, usage, service,
     CASE
