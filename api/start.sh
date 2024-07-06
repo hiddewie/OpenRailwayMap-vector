3160f9871c1b4afdf6747fac3bf79f217522ccf3
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-07-06 17:28:29 +0200

    tune postgres

--- api/start.sh
+++ api/start.sh
@@ -4,7 +4,25 @@ set -e
 set -o pipefail
 
 echo 'starting postgres'
-docker-entrypoint.sh postgres 1>/dev/stdout 2>/dev/stderr &
+# Optimized for low memory usage (150MB), see https://pgtune.leopard.in.ua/#/
+docker-entrypoint.sh postgres \
+  -c max_connections=200 \
+  -c shared_buffers=38400kB \
+  -c effective_cache_size=115200kB \
+  -c maintenance_work_mem=9600kB \
+  -c checkpoint_completion_target=0.9 \
+  -c wal_buffers=1152kB \
+  -c default_statistics_target=100 \
+  -c random_page_cost=1.1 \
+  -c effective_io_concurrency=200 \
+  -c work_mem=96kB \
+  -c huge_pages=off \
+  -c min_wal_size=1GB \
+  -c max_wal_size=4GB \
+  1>/dev/stdout \
+  2>/dev/stderr \
+  &
+
 sleep 1
 
 echo 'waiting until postgres ready'
