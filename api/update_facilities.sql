80842345f694086fc21d322c30780404455c30b0
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-07-10 20:18:16 +0200

    remove update materialized view SQL

--- api/update_facilities.sql
+++
@@ -1,5 +0,0 @@
--- SPDX-License-Identifier: GPL-2.0-or-later
--- Flush data updates to the materialized views.
-
-REFRESH MATERIALIZED VIEW openrailwaymap_ref;
-REFRESH MATERIALIZED VIEW openrailwaymap_facilities_for_search;
--- api/update_milestones.sql
+++
@@ -1,4 +0,0 @@
--- SPDX-License-Identifier: GPL-2.0-or-later
--- Flush data updates to the materialized view.
-
-REFRESH MATERIALIZED VIEW openrailwaymap_milestones;
