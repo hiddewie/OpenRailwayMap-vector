f8f2f653689e1761017c259a350bcff0c4fc2b20
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-07-14 19:14:26 +0200

    Use python formatted strings in tests

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -15,7 +15,7 @@ class TestCase():
         self.response = kwargs["response"]
 
     def run(self, api):
-        url = "{}{}".format(api, self.request["path"])
+        url = f"{api}{self.request["path"]}"
         r = requests.get(url, params=self.request.get("params"))
         self.test_equals("status code", r.status_code, self.response.get("status", 200))
         response_data = r.json()
@@ -32,47 +32,47 @@ class TestCase():
                 expected = expected_results[i]
                 got = response_data[i]
                 if type(expected) is not dict:
-                    self.fail("Expected result must be a dict, not {}: {}".format(type(expected), expected))
+                    self.fail(f"Expected result must be a dict, not {type(expected)}: {expected}")
                 for key, expected_value in expected.items():
                     if match_type == "str_equals":
-                        self.test_str_equals("result['{}']['{}']".format(i + 1, key), expected_value, response_data[i][key])
+                        self.test_str_equals(f"result['{i + 1}']['{key}']", expected_value, response_data[i][key])
                     elif match_type == "regexp_full":
-                        self.test_regexp_full("result['{}']['{}']".format(i + 1, key), expected_value, response_data[i][key])
+                        self.test_regexp_full(f"result['{i + 1}']['{key}']", expected_value, response_data[i][key])
                     else:
-                        self.fail("Unsupported comparison {}".format(match_type))
+                        self.fail(f"Unsupported comparison {match_type}")
         self.passed() 
 
     def test_is_type(self, obj, expected_type):
         if type(obj) is not expected_type:
-            self.fail(" {} but {} was expected.".format(status))
+            self.fail(f" {type(obj)} but {expected_type} was expected.")
 
     def test_regexp_full(self, what, regexp, got):
         """Compare provided string with a regular expression. The expression must match the full string.
         """
         if not re.fullmatch(regexp, got):
-            print("pattern: {}  string: {}".format(regexp, got))
-            self.fail("{}: {} does not match regular expression '{}'".format(what, got, regexp))
+            print(f"pattern: {regexp}  string: {got}")
+            self.fail(f"{what}: {got} does not match regular expression '{regexp}'")
 
     def test_equals(self, what, expected, got):
         if got != expected:
-            self.fail("{}: {} but {} was expected.".format(what, got, expected))
+            self.fail(f"{what}: {got} but {expected} was expected.")
 
     def test_str_equals(self, what, expected, got):
         if str(got) != str(expected):
-            self.fail("{}: {} but {} was expected.".format(what, got, expected))
+            self.fail(f"{what}: {got} but {expected} was expected.")
 
     def print_traceback(self, color):
         for line in traceback.format_stack():
             print(colored(line.strip(), color))
 
     def fail(self, message):
-        text = "ERROR: Test '{}' failed.\n{}".format(self.name, message)
+        text = f"ERROR: Test '{self.name}' failed.\n{message}"
         print(colored(text, 'red'))
         self.print_traceback('red')
         sys.exit(1)
     
     def passed(self):
-        text = "PASSED: {}".format(self.name)
+        text = f"PASSED: {self.name}"
         print(colored(text, 'green'))
  
 
