a2f547b9adc2e4be32a7fe6336ffcc432f84ca66
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-07-21 21:35:17 +0200

    Clean up

--- api/api.py
+++ api/api.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-# SPDX-License-Identifier: GPL-2.0-or-later
 import contextlib
 import os
 from typing import Annotated
@@ -12,18 +10,6 @@ from openrailwaymap_api.facility_api import FacilityAPI
 from openrailwaymap_api.milestone_api import MilestoneAPI
 from openrailwaymap_api.status_api import StatusAPI
 
-
-# import psycopg2
-# import psycopg2.extras
-# import json
-# import sys
-# import os
-# from werkzeug.exceptions import HTTPException, NotFound, InternalServerError
-# from werkzeug.routing import Map, Rule
-# from werkzeug.wrappers import Request, Response
-# from openrailwaymap_api.facility_api import FacilityAPI
-# from openrailwaymap_api.milestone_api import MilestoneAPI
-# from openrailwaymap_api.status_api import StatusAPI
 
 @contextlib.asynccontextmanager
 async def lifespan(app):
@@ -50,7 +36,8 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-DEFAULT_LIMIT = 20
+DEFAULT_FACILITY_LIMIT = 20
+DEFAULT_MILESTONE_LIMIT = 2
 MIN_LIMIT = 1
 MAX_LIMIT = 200
 
@@ -67,7 +54,7 @@ async def facility(
         name: Annotated[str | None, Query()] = None,
         ref: Annotated[str | None, Query()] = None,
         uic_ref: Annotated[str | None, Query()] = None,
-        limit: Annotated[int, Query(ge=MIN_LIMIT, le=MAX_LIMIT)] = DEFAULT_LIMIT,
+        limit: Annotated[int, Query(ge=MIN_LIMIT, le=MAX_LIMIT)] = DEFAULT_FACILITY_LIMIT,
 ):
     api = FacilityAPI(app.state.database)
     return await api(q=q, name=name, ref=ref, uic_ref=uic_ref, limit=limit)
@@ -77,63 +64,7 @@ async def facility(
 async def milestone(
         ref: Annotated[str, Query()],
         position: Annotated[float, Query()],
-        limit: Annotated[int | None, Query(ge=MIN_LIMIT, le=MAX_LIMIT)] = DEFAULT_LIMIT,
+        limit: Annotated[int | None, Query(ge=MIN_LIMIT, le=MAX_LIMIT)] = DEFAULT_MILESTONE_LIMIT,
 ):
     api = MilestoneAPI(app.state.database)
     return await api(ref=ref, position=position, limit=limit)
-
-#
-# def connect_db():
-#     conn = psycopg2.connect(dbname=os.environ['POSTGRES_DB'], user=os.environ['POSTGRES_USER'], host=os.environ['POSTGRES_HOST'])
-#     return conn
-#
-# class OpenRailwayMapAPI:
-#
-#     db_conn = connect_db()
-#
-#     def __init__(self):
-#         self.url_map = Map([
-#             Rule('/api/facility', endpoint=FacilityAPI, methods=('GET',)),
-#             Rule('/api/milestone', endpoint=MilestoneAPI, methods=('GET',)),
-#             Rule('/api/status', endpoint=StatusAPI, methods=('GET',)),
-#         ])
-#
-#     def ensure_db_connection_alive(self):
-#         if self.db_conn.closed != 0:
-#             self.db_conn = connect_db()
-#
-#     def dispatch_request(self, environ, start_response):
-#         request = Request(environ)
-#         urls = self.url_map.bind_to_environ(environ)
-#         response = None
-#         try:
-#             endpoint, args = urls.match()
-#             self.ensure_db_connection_alive()
-#             response = endpoint(self.db_conn)(request.args)
-#         except HTTPException as e:
-#             return e
-#         except Exception as e:
-#             print('Error during request:', e, file=sys.stderr)
-#             return InternalServerError()
-#         finally:
-#             if not response:
-#                 self.db_conn.close()
-#                 self.db_conn = connect_db()
-#         return response
-#
-#     def wsgi_app(self, environ, start_response):
-#         request = Request(environ)
-#         response = self.dispatch_request(request)
-#         return response(environ, start_response)
-#
-#
-# def application(environ, start_response):
-#     openrailwaymap_api = OpenRailwayMapAPI()
-#     response = openrailwaymap_api.dispatch_request(environ, start_response)
-#     return response(environ, start_response)
-#
-#
-# if __name__ == '__main__':
-#     openrailwaymap_api = OpenRailwayMapAPI()
-#     from werkzeug.serving import run_simple
-#     run_simple('::', int(os.environ['PORT']), application, use_debugger=True, use_reloader=True)
