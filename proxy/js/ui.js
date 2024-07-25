842c7dbb38eced775d353ed249b411b58a450384
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-07-25 21:07:24 +0200

    edit icon

--- proxy/js/ui.js
+++ proxy/js/ui.js
@@ -559,8 +559,24 @@ map.on('click', 'search', (e) => {
   const feature = e.features[0];
   const coordinates = feature.geometry.coordinates.slice();
   const properties = feature.properties;
+  const icon = properties.railway ? {
+    station: '(s)',
+    halt: '(h)',
+    tram_stop: '(t)',
+    service_station: '(ss)',
+    yard: '(y)',
+    junction: '(j)',
+    spur_junction: '',
+    crossover: '',
+    site: '',
+  }[properties.railway] : null;
+  const editIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="auto" height="16" fill="#007bff" viewBox="0 0 24 24"><path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>';
   const content = `
-    <h6><span title="${properties.railway}">(i)</span> <a href="https://www.openstreetmap.org/node/${properties.osm_id}" target="_blank">${properties.name}</a> <a href="https://www.openstreetmap.org/edit/node=${properties.osm_id}" target="_blank">${properties.name}</a>(e)</a></h6>
+    <h6>
+      ${icon ? `<span title="${properties.railway}">${icon}</span>` : ''}
+      <a title="View" href="https://www.openstreetmap.org/node/${properties.osm_id}" target="_blank">${properties.name}</a> 
+      <a title="Edit" href="https://www.openstreetmap.org/edit?node=${properties.osm_id}" target="_blank">${editIcon}</a>
+    </h6>
     <h6>
       ${properties.railway_ref ? `<span class="badge badge-pill badge-light">reference: <span class="text-monospace">${properties.railway_ref}</span></span>` : ''} 
       ${properties.ref ? `<span class="badge badge-pill badge-light">reference: <span class="text-monospace">${properties.ref}</span></span>` : ''} 
