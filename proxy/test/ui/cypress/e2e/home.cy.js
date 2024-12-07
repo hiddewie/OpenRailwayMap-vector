f3223f54d02ed1bb0e7f3b42406c22d14c15a205
Hidde Wieringa <hidde@hiddewieringa.nl>
2024-12-07 21:09:55 +0100

    config backdrop ID selector

--- proxy/test/ui/cypress/e2e/home.cy.js
+++ proxy/test/ui/cypress/e2e/home.cy.js
@@ -55,7 +55,7 @@ describe('home page', () => {
 
     cy.screenshot()
 
-    cy.contains('Map configuration').get('.btn-close').click()
+    cy.get('#configuration-backdrop .btn-close').click()
     cy.contains('Map configuration').should('not.be.visible')
 
     cy.screenshot()
