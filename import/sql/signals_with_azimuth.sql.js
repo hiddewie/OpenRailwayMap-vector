d198042a8a204fd9ccb3445551bed8df5e69e590
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-01-12 20:18:36 +0100

    generate unknown icons

--- import/sql/signals_with_azimuth.sql.js
+++ import/sql/signals_with_azimuth.sql.js
@@ -41,6 +41,9 @@ CREATE OR REPLACE VIEW signals_with_azimuth_view AS
           ${feature.icon.default ? `ELSE '${feature.icon.default}'` : ''}
         END` : `'${feature.icon.default}'`}
     `).join('')}
+      -- Unknown signal
+      WHEN "railway:signal:${type.type}" IS NOT NULL THEN
+        'general/signal-unknown-${type.type}'
     END as feature_${type.type},
     `).join('')}
       
