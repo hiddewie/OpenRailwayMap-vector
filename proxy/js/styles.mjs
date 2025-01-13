8c54f48b8ac6f0e31aa809c5870d8009eec7a46f
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-01-13 19:28:11 +0100

    Fix `way_length` using meter unit
    Since https://github.com/hiddewie/OpenRailwayMap-vector/pull/197
    
    `way_length` is now in meters instead of degrees.

--- proxy/js/styles.mjs
+++ proxy/js/styles.mjs
@@ -842,7 +842,7 @@ const railwayLine = (theme, text, layers) => [
           ['>=',
             ['get', 'way_length'],
             ['interpolate', ["exponential", .5], ['zoom'],
-              8, 0.015,
+              8, 1500,
               16, 0
             ],
           ],
@@ -869,9 +869,10 @@ const railwayLine = (theme, text, layers) => [
         filter: ['all',
           ['==', ['get', 'state'], 'present'],
           ['get', 'bridge'],
-          ['>=', ['get', 'way_length'],
+          ['>=',
+            ['get', 'way_length'],
             ['interpolate', ["exponential", .5], ['zoom'],
-              8, 0.015,
+              8, 1500,
               16, 0
             ],
           ],
