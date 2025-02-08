15ad9ba778cae628e12ba0f5775de376c51d97e6
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-02-08 19:46:45 +0100

    refactor startup file

--- import/docker-startup.sh
+++ import/docker-startup.sh
@@ -10,20 +10,20 @@ OSM2PGSQL_FILTERED_FILE="/data/filtered/${OSM2PGSQL_DATAFILE:-data.osm.pbf}"
 
 PSQL="psql --dbname gis --variable ON_ERROR_STOP=on --pset pager=off"
 
-if [[ ! -f "$OSM2PGSQL_FILTERED_FILE" ]]; then
-  echo "Filtering data from $OSM2PGSQL_INPUT_FILE to $OSM2PGSQL_FILTERED_FILE"
+function filter_data() {
+  if [[ ! -f "$OSM2PGSQL_FILTERED_FILE" ]]; then
+    echo "Filtering data from $OSM2PGSQL_INPUT_FILE to $OSM2PGSQL_FILTERED_FILE"
 
-  mkdir -p "$(dirname "$OSM2PGSQL_FILTERED_FILE")"
+    mkdir -p "$(dirname "$OSM2PGSQL_FILTERED_FILE")"
 
-  osmium tags-filter \
-    "$OSM2PGSQL_INPUT_FILE" \
-    --output "$OSM2PGSQL_FILTERED_FILE" \
-    --expressions osmium-tags-filter
-fi
+    osmium tags-filter \
+      "$OSM2PGSQL_INPUT_FILE" \
+      --output "$OSM2PGSQL_FILTERED_FILE" \
+      --expressions osmium-tags-filter
+  fi
+}
 
-case "$1" in
-import)
-
+function import_db() {
   echo "Creating default database"
   psql -c "SELECT 1 FROM pg_database WHERE datname = 'gis';" | grep -q 1 || createdb gis
   $PSQL -c 'CREATE EXTENSION IF NOT EXISTS postgis;'
@@ -37,17 +37,15 @@ import)
   osm2pgsql \
     --create \
     --database gis \
-    --slim \
+    --drop \
     --output flex \
     --style openrailwaymap.lua \
     --cache "${OSM2PGSQL_CACHE:-256}" \
     --number-processes "${OSM2PGSQL_NUMPROC:-4}" \
     "$OSM2PGSQL_FILTERED_FILE"
+}
 
-  ;;
-
-update)
-
+function update_datafile() {
   # This command may exit with non-zero exit codes
   #   in case there are more updates, or if no updates were performed
   # This uses the replication server as defined in the input file
@@ -69,54 +67,59 @@ update)
     --output "/tmp/data.osm.pbf" \
     --expressions osmium-tags-filter \
       && mv "/tmp/data.osm.pbf" "$OSM2PGSQL_FILTERED_FILE"
+}
 
-  ;;
+function reduce_data() {
+  # Remove platforms which are not near any railway line, and also not part of any railway route
+  $PSQL -c "delete from platforms p where not exists(select * from routes r where r.platform_ref_ids @> Array[-p.osm_id]) and not exists(select * from railway_line l where st_dwithin(p.way, l.way, 20));"
+}
 
-refresh)
+function create_update_functions_views() {
+  echo "Post processing imported data"
+  $PSQL -f sql/functions.sql
+  $PSQL -f sql/signals_with_azimuth.sql
+  $PSQL -f sql/get_station_importance.sql
+  $PSQL -f sql/tile_views.sql
+}
 
-  echo "Refreshing tables and views"
+function refresh_materialized_views() {
+  echo "Updating materialized views"
+  $PSQL -f sql/update_signals_with_azimuth.sql
+  $PSQL -f sql/update_station_importance.sql
+}
 
-  ;;
+function print_summary() {
+  $PSQL --tuples-only -c "with bounds as (SELECT st_transform(st_setsrid(ST_Extent(way), 3857), 4326) as table_extent FROM railway_line) select '[[' || ST_XMin(table_extent) || ', ' || ST_YMin(table_extent) || '], [' || ST_XMax(table_extent) || ', ' || ST_YMax(table_extent) || ']]' from bounds;" > /data/import/bounds.json
+  echo "Import bounds: $(cat /data/import/bounds.json)"
 
-*)
-
-  echo "Invalid argument '$1'. Supported: import, update, refresh"
-  exit 1
-
-  ;;
-
-esac
-
-# Clear all tags from the Osm2Psql tables
-$PSQL -c "update planet_osm_nodes set tags = null where tags is not null;"
-$PSQL -c "update planet_osm_ways set tags = null where tags is not null;"
-$PSQL -c "update planet_osm_rels set tags = null where tags is not null;"
-# Remove platforms which are not near any railway line, and also not part of any railway route
-$PSQL -c "delete from platforms p where not exists(select * from routes r where r.platform_ref_ids @> Array[-p.osm_id]) and not exists(select * from railway_line l where st_dwithin(p.way, l.way, 20));"
-
-echo "Post processing imported data"
-$PSQL -f sql/functions.sql
-$PSQL -f sql/signals_with_azimuth.sql
-$PSQL -f sql/get_station_importance.sql
-$PSQL -f sql/tile_views.sql
+  echo "Database summary"
+  $PSQL -c "select concat(relname, ' (', relkind ,')') as name, pg_size_pretty(pg_table_size(oid)) as size from pg_class where relkind in ('m', 'r', 'i') and relname not like 'pg_%' order by pg_table_size(oid) desc;"
+  $PSQL -c "select pg_size_pretty(SUM(pg_table_size(oid))) as size from pg_class where relkind in ('m', 'r', 'i') and relname not like 'pg_%';"
+}
 
 case "$1" in
 import)
 
-  echo "Skipping updating of materialized views"
+  filter_data
+  import_db
+  reduce_data
+  create_update_functions_views
+  print_summary
 
   ;;
 
 update)
 
-  # Fallthrough
-  ;&
+  filter_data
+  update_datafile
+
+  ;;
 
 refresh)
 
-  echo "Updating materialized views"
-  $PSQL -f sql/update_signals_with_azimuth.sql
-  $PSQL -f sql/update_station_importance.sql
+  create_update_functions_views
+  refresh_materialized_views
+  print_summary
 
   ;;
 
@@ -128,13 +131,3 @@ refresh)
   ;;
 
 esac
-
-echo "Vacuuming database"
-$PSQL -c "VACUUM FULL;"
-
-$PSQL --tuples-only -c "with bounds as (SELECT st_transform(st_setsrid(ST_Extent(way), 3857), 4326) as table_extent FROM railway_line) select '[[' || ST_XMin(table_extent) || ', ' || ST_YMin(table_extent) || '], [' || ST_XMax(table_extent) || ', ' || ST_YMax(table_extent) || ']]' from bounds;" > /data/import/bounds.json
-echo "Import bounds: $(cat /data/import/bounds.json)"
-
-echo "Database summary"
-$PSQL -c "select concat(relname, ' (', relkind ,')') as name, pg_size_pretty(pg_table_size(oid)) as size from pg_class where relkind in ('m', 'r', 'i') and relname not like 'pg_%' order by pg_table_size(oid) desc;"
-$PSQL -c "select pg_size_pretty(SUM(pg_table_size(oid))) as size from pg_class where relkind in ('m', 'r', 'i') and relname not like 'pg_%';"
