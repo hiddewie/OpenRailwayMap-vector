6e6c4d7464f326f98c88862ecccefcf132f5f457
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-03-22 21:02:17 +0100

    render details in popup

--- proxy/js/features.mjs
+++ proxy/js/features.mjs
@@ -266,8 +266,20 @@ const features = {
       }
     },
     properties: {
+      ref: {
+        name: 'Reference',
+      },
+      type: {
+        name: 'Type',
+      },
+      turnout_side: {
+        name: 'Turnout side',
+      },
       local_operated: {
         name: 'Operated locally',
+      },
+      resetting: {
+        name: 'Resetting',
       },
     },
   },
