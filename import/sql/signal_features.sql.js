1d758e7fbaa20c3e2e9251c12715efc8e67fb030
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-05-17 18:39:03 +0200

    electrification signals to function

--- import/sql/signal_features.sql.js
+++ import/sql/signal_features.sql.js
@@ -289,34 +289,79 @@ DO $do$ BEGIN
   $$::json || '$tj$';
 END $do$;
 
-
 --- Electrification ---
 
-CREATE OR REPLACE VIEW electrification_signals AS
-  SELECT
-    id,
-    osm_id,
-    way,
-    direction_both,
-    ref,
-    caption,
-    deactivated,
-    voltage,
-    frequency,
-    wikidata,
-    wikimedia_commons,
-    image,
-    mapillary,
-    wikipedia,
-    note,
-    description,
-    azimuth,${signals_railway_signals.tags.map(tag => `
-    ${tag.type === 'array' ? `array_to_string("${tag.tag}", U&'\\001E') as "${tag.tag}"` : `"${tag.tag}"`},`).join('')}
-    features[1] as feature,
-    type as type
-  FROM signal_features
-  WHERE layer = 'electrification'
-  ORDER BY rank NULLS FIRST;
+CREATE OR REPLACE FUNCTION electrification_signals(z integer, x integer, y integer)
+  RETURNS bytea
+  LANGUAGE SQL
+  IMMUTABLE
+  STRICT
+  PARALLEL SAFE
+  RETURN (
+    SELECT
+      ST_AsMVT(tile, 'electrification_signals', 4096, 'way')
+    FROM (
+      SELECT
+        id,
+        osm_id,
+        ST_AsMVTGeom(way, ST_TileEnvelope(z, x, y), extent => 4096) AS way,
+        direction_both,
+        ref,
+        caption,
+        deactivated,
+        voltage,
+        frequency,
+        wikidata,
+        wikimedia_commons,
+        image,
+        mapillary,
+        wikipedia,
+        note,
+        description,
+        azimuth,${signals_railway_signals.tags.map(tag => `
+        ${tag.type === 'array' ? `array_to_string("${tag.tag}", U&'\\001E') as "${tag.tag}"` : `"${tag.tag}"`},`).join('')}
+        features[1] as feature,
+        type as type
+      FROM signal_features
+      WHERE way && ST_TileEnvelope(z, x, y)
+        AND layer = 'electrification'
+      ORDER BY rank NULLS FIRST
+    ) as tile
+    WHERE way IS NOT NULL
+  );
+
+-- Function metadata
+DO $do$ BEGIN
+  EXECUTE 'COMMENT ON FUNCTION electrification_signals IS $tj$' || $$
+  {
+    "vector_layers": [
+      {
+        "id": "electrification_signals",
+        "fields": {
+          "id": "integer",
+          "osm_id": "integer",
+          "azimuth": "number",
+          "direction_both": "boolean",
+          "ref": "string",
+          "caption": "string",
+          "deactivated": "boolean",
+          "frequency": "number",
+          "voltage": "integer",
+          "wikidata": "string",
+          "wikimedia_commons": "string",
+          "image": "string",
+          "mapillary": "string",
+          "wikipedia": "string",
+          "note": "string",
+          "description": "string",${signals_railway_signals.tags.map(tag => `
+          "${tag.tag}": "${tag.type === 'boolean' ? `boolean` : `string`}",`).join('')}
+          "feature": "string"
+        }
+      }
+    ]
+  }
+  $$::json || '$tj$';
+END $do$;
 `
 
 console.log(sql);
