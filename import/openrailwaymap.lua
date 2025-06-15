2df8f49fa500c8731721e6abd87fdc3725a549ae
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-06-15 21:34:45 +0200

    move function

--- import/openrailwaymap.lua
+++ import/openrailwaymap.lua
@@ -611,6 +611,37 @@ function railway_feature_and_state(tags, railway_value_func)
   return nil, nil
 end
 
+local vehicles = {'train', 'subway', 'light_rail', 'monorail', 'funicular', 'miniature'}
+function station_type(tags)
+  -- Determine the type of station
+  local feature_stations = {}
+  local has_entries = false
+
+  if tags.station then
+    for station in string.gmatch(tags.station, '[^;]+') do
+      feature_stations[station] = true
+      has_entries = true
+    end
+  else
+    for _, vehicle in ipairs(vehicles) do
+      if tags[vehicle] == 'yes' then
+        feature_stations[vehicle] = true
+        has_entries = true
+      end
+    end
+  end
+
+  if not has_entries then
+    if tags.railway == 'tram_stop' then
+      feature_stations['tram'] = true
+    else
+      feature_stations['train'] = true
+    end
+  end
+
+  return feature_stations
+end
+
 local railway_station_values = osm2pgsql.make_check_values_func({'station', 'halt', 'tram_stop', 'service_station', 'yard', 'junction', 'spur_junction', 'crossover', 'site'})
 local railway_poi_values = osm2pgsql.make_check_values_func(tag_functions.poi_railway_values)
 local railway_signal_values = osm2pgsql.make_check_values_func({'signal', 'buffer_stop', 'derail', 'vacancy_detection'})
@@ -623,7 +654,6 @@ local entrance_types = {
   subway_entrance = 'subway',
   train_station_entrance = 'train',
 }
-local vehicles = {'train', 'subway', 'light_rail', 'monorail', 'funicular', 'miniature'}
 function osm2pgsql.process_node(object)
   local tags = object.tags
   local wikimedia_commons, image = wikimedia_commons_or_image(tags.wikimedia_commons, tags.image)
@@ -659,29 +689,7 @@ function osm2pgsql.process_node(object)
       end
     end
 
-    -- Determine the type of station
-    local feature_stations = {}
-    if tags.station then
-      for station in string.gmatch(tags.station, '[^;]+') do
-        feature_stations[station] = true
-      end
-    end
-
-    for _, vehicle in ipairs(vehicles) do
-      if tags[vehicle] == 'yes' then
-        feature_stations[vehicle] = true
-      end
-    end
-
-    if #feature_stations == 0 then
-      if tags.railway == 'tram_stop' then
-        feature_stations['tram'] = true
-      else
-        feature_stations['train'] = true
-      end
-    end
-
-    for station, _ in pairs(feature_stations) do
+    for station, _ in pairs(station_type(tags)) do
       stations:insert({
         way = object:as_point(),
         feature = station_feature,
