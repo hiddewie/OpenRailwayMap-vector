71d49df94c926405af0d45187594915c77e3d9b5
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-07-16 22:29:02 +0200

    simplify openrailwaymap_milestones view

--- import/sql/api_milestone_views.sql
+++ import/sql/api_milestone_views.sql
@@ -5,7 +5,6 @@ CREATE MATERIALIZED VIEW openrailwaymap_milestones AS
   SELECT DISTINCT ON (osm_id)
     osm_id,
     position,
-    precision,
     railway,
     name,
     ref,
@@ -23,7 +22,6 @@ CREATE MATERIALIZED VIEW openrailwaymap_milestones AS
     SELECT
       osm_id,
       position,
-      precision,
       railway,
       name,
       ref,
@@ -40,8 +38,7 @@ CREATE MATERIALIZED VIEW openrailwaymap_milestones AS
     FROM (
       SELECT
         osm_id,
-        railway_api_valid_float(unnest(string_to_array(railway_position, ';'))) AS position,
-        1::SMALLINT AS precision,
+        position_numeric AS position,
         railway,
         name,
         ref,
@@ -56,32 +53,10 @@ CREATE MATERIALIZED VIEW openrailwaymap_milestones AS
         note,
         description
       FROM railway_positions
-      WHERE railway_position IS NOT NULL
-
-      UNION ALL
-
-      SELECT
-        osm_id,
-        railway_api_valid_float(unnest(string_to_array(railway_position_exact, ';'))) AS position,
-        3::SMALLINT AS precision,
-        railway,
-        name,
-        ref,
-        operator,
-        way AS geom,
-        wikidata,
-        wikimedia_commons,
-        wikimedia_commons_file,
-        image,
-        mapillary,
-        wikipedia,
-        note,
-        description
-      FROM railway_positions
-      WHERE railway_position_exact IS NOT NULL
+      WHERE position_numeric IS NOT NULL
     ) AS features_with_position
     WHERE position IS NOT NULL
-    ORDER BY osm_id, precision DESC
+    ORDER BY osm_id DESC
   ) AS duplicates_merged;
 
 CREATE INDEX openrailwaymap_milestones_geom_idx
