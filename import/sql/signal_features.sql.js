3061114150c831070a1d1efe66d4a454874d6b70
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-07-19 21:31:36 +0200

    Position signal features on left/right side of rail
    Part of #270

--- import/sql/signal_features.sql.js
+++ import/sql/signal_features.sql.js
@@ -155,29 +155,69 @@ CREATE OR REPLACE VIEW signal_features_view AS
       MAX(rank) as rank
     FROM signals_with_features_1 sf
     GROUP BY signal_id, layer
+  ),
+  -- Find signal azimuth
+  signals_with_azimuth AS (
+    SELECT
+      id,
+      s.way,
+      "railway:signal:position",
+      ST_Azimuth(
+        st_lineinterpolatepoint(sl.way, greatest(0, st_linelocatepoint(sl.way, ST_ClosestPoint(sl.way, s.way)) - 0.01)),
+        st_lineinterpolatepoint(sl.way, least(1, st_linelocatepoint(sl.way, ST_ClosestPoint(sl.way, s.way)) + 0.01))
+      ) + (CASE WHEN signal_direction = 'backward' THEN pi() ELSE 0.0 END) as azimuth
+    FROM signals s
+    LEFT JOIN LATERAL (
+      SELECT line.way as way
+      FROM railway_line line
+      WHERE st_dwithin(s.way, line.way, 10) AND line.feature IN ('rail', 'tram', 'light_rail', 'subway', 'narrow_gauge', 'monorail', 'miniature', 'funicular')
+      ORDER BY s.way <-> line.way
+      LIMIT 1
+    ) as sl ON true
+  ),
+  -- Translate signal with azimuth
+  translated_signals_with_azimuth AS (
+    SELECT
+      id,
+      azimuth,
+      ST_translate(
+        way, 
+        1 * cos(azimuth + (CASE "railway:signal:position" WHEN 'right' THEN 1.0 WHEN 'left' THEN -1.0 ELSE 0.0 END) * pi() / 2.0),
+        1 * sin(azimuth + (CASE "railway:signal:position" WHEN 'right' THEN 1.0 WHEN 'left' THEN -1.0 ELSE 0.0 END) * pi() / 2.0)
+      ) as way
+    FROM signals_with_azimuth
   )
-  -- Calculate signal-specific details like fields, azimuth and features
+  -- Aggregate calculated properties and features for final view
   SELECT
-    s.*,
+    s.id,
+    s.osm_id,
+    sa.way,
+    s.railway,
+    s.ref,
+    s.ref_multiline,
+    s.caption,
+    s.deactivated,
+    s.position,
+    s.wikidata,
+    s.wikimedia_commons,
+    s.wikimedia_commons_file,
+    s.image,
+    s.mapillary,
+    s.wikipedia,
+    s.note,
+    s.description,${signals_railway_signals.tags.map(tag => `
+    s."${tag.tag}",`).join('')}
     sf.type,
     sf.layer,
     sf.features,
     sf.rank,
     (signal_direction = 'both') as direction_both,
-    degrees(ST_Azimuth(
-      st_lineinterpolatepoint(sl.way, greatest(0, st_linelocatepoint(sl.way, ST_ClosestPoint(sl.way, s.way)) - 0.01)),
-      st_lineinterpolatepoint(sl.way, least(1, st_linelocatepoint(sl.way, ST_ClosestPoint(sl.way, s.way)) + 0.01))
-    )) + (CASE WHEN signal_direction = 'backward' THEN 180.0 ELSE 0.0 END) as azimuth
+    degrees(sa.azimuth) as azimuth
   FROM signals_with_features sf
   JOIN signals s
     ON s.id = sf.signal_id
-  LEFT JOIN LATERAL (
-    SELECT line.way as way
-    FROM railway_line line
-    WHERE st_dwithin(s.way, line.way, 10) AND line.feature IN ('rail', 'tram', 'light_rail', 'subway', 'narrow_gauge', 'monorail', 'miniature', 'funicular')
-    ORDER BY s.way <-> line.way
-    LIMIT 1
-  ) as sl ON true
+  JOIN translated_signals_with_azimuth sa
+    ON sa.id = sf.signal_id
   WHERE
     (railway IN ('signal', 'buffer_stop') AND signal_direction IS NOT NULL)
       OR railway IN ('derail', 'vacancy_detection');
