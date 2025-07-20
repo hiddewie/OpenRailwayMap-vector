141597da2ffe38205096d01d10bc891f9814f90c
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-07-20 20:56:12 +0200

    Change defaults of background map
    Users that have saved their configuration have no different behaviour.
    
    For new users, the default background map will have a saturation of 0.2 (from 0.0) and opacity of 0.5 (from 1.0).
    
    Before:
    
    After

--- proxy/js/ui.js
+++ proxy/js/ui.js
@@ -585,8 +585,8 @@ function updateBackgroundMapContainer() {
 }
 
 const defaultConfiguration = {
-  backgroundSaturation: 0.0,
-  backgroundOpacity: 1.0,
+  backgroundSaturation: 0.2,
+  backgroundOpacity: 0.5,
   backgroundType: 'raster',
   backgroundUrl: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
   theme: 'system',
