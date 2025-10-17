31c3a7a6da12b2a1f0cc4df2aba3879746b454e0
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-10-17 23:15:43 +0200

    more tests

--- import/test/test_openrailwaymap.lua
+++ import/test/test_openrailwaymap.lua
@@ -46,6 +46,22 @@ local position1, position1_exact, position1_line_positions = find_position_tags(
 })
 assert_eq(position1, "123")
 assert_eq(position1_exact, "123.0")
-assert_eq(position1_line_positions, {["L123"] = "123.0", ["AA1"] = "1.0"})
+assert_eq(position1_line_positions, {L123 = "123.0", AA1 = "1.0"})
 
--- parse_railway_positions
+local positions1 = parse_railway_positions("1.0", "1.05", {})
+assert_eq(positions1, {{zero = 1, numeric = 1.05, text = "1.0", type = "km", exact = "1.05"}})
+
+local positions2 = parse_railway_positions("1.0", nil, {})
+assert_eq(positions2, {{zero = 1, numeric = 1.0, text = "1.0", type = "km", exact = nil}})
+
+local positions3 = parse_railway_positions(nil, "1.05", {})
+assert_eq(positions3, {{numeric = 1.05, text = "1.05", type = "km", exact = nil}})
+
+local positions4 = parse_railway_positions("1.05", "1.05", {L123 = "1.05"})
+assert_eq(positions4, {{numeric = 1.05, text = "1.05", type = "km", exact = "1.05", line = "L123"}})
+
+local positions5 = parse_railway_positions("1.3", "1.05", {L123 = "1.05"})
+assert_eq(positions5, {
+  {numeric = 1.3, text = "1.3", type = "km"},
+  {numeric = 1.05, text = "1.05", type = "km", exact = "1.05", line = "L123"},
+})
