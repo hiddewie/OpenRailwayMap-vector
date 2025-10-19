56d44358483fc5cb3ff1c659363f0417f31781bc
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-10-19 11:24:23 +0200

    signal box

--- import/test/test_import_node.lua
+++ import/test/test_import_node.lua
@@ -7,18 +7,35 @@ require('mock_osm2psql')
 
 local openrailwaymap = require('openrailwaymap')
 
-local node = {
+-- Boxes
+osm2pgsql.process_node({
+  tags = {
+    ['railway'] = 'signal_box',
+    ['railway:position'] = '1.2',
+    ['railway:position:exact'] = '1.2345',
+    name = 'name',
+    ['railway:ref'] = 'ref',
+    operator = 'operator',
+  },
+  as_point = function () end,
+})
+assert.eq(osm2pgsql.get_and_clear_imported_data(), {
+  boxes = {
+    { way_area = 0, feature = 'signal_box', ref = 'ref', name = 'name', operator = 'operator', position = '{"1.2 @ 1.2345 (km)"}' },
+  },
+})
+
+-- Milestones
+osm2pgsql.process_node({
   tags = {
     ['railway'] = 'milestone',
     ['railway:position'] = '1.2',
     ['railway:position:exact'] = '1.2345',
   },
   as_point = function () end,
-}
-osm2pgsql.process_node(node)
-local imported = osm2pgsql.get_and_clear_imported_data()
-assert.eq(imported, {
+})
+assert.eq(osm2pgsql.get_and_clear_imported_data(), {
   railway_positions = {
-    { railway = 'milestone', position_text = '1.2', position_exact = '1.2345', zero = false, type = 'km', position_numeric = 1.2345}
+    { railway = 'milestone', position_text = '1.2', position_exact = '1.2345', zero = false, type = 'km', position_numeric = 1.2345 },
   },
 })
