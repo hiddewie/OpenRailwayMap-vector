40e1e5da32d05c8434987910ec03083a02842a76
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-10-24 20:12:20 +0200

    entrances

--- import/test/test_import_node.lua
+++ import/test/test_import_node.lua
@@ -910,6 +910,36 @@ assert.eq(osm2pgsql.get_and_clear_imported_data(), {
   },
 })
 
+-- Entrances
+
+osm2pgsql.process_node({
+  tags = {
+    ['railway'] = 'subway_entrance',
+    ['name'] = 'name',
+    ['ref'] = '47',
+  },
+  as_point = function () end,
+})
+assert.eq(osm2pgsql.get_and_clear_imported_data(), {
+  station_entrances = {
+    { type = 'subway', name = 'name', ref = '47' },
+  },
+})
+
+osm2pgsql.process_node({
+  tags = {
+    ['railway'] = 'train_station_entrance',
+    ['name'] = 'name',
+    ['ref'] = '47',
+  },
+  as_point = function () end,
+})
+assert.eq(osm2pgsql.get_and_clear_imported_data(), {
+  station_entrances = {
+    { type = 'train', name = 'name', ref = '47' },
+  },
+})
+
 -- Milestones
 
 osm2pgsql.process_node({
