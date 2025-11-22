8b02f6fac6d35ef1e6d55dc329859014ef123835
hidde.wieringa <hidde.wieringa@nedap.com>
2025-11-22 21:54:26 +0100

    rename route count to importance

--- import/sql/tile_views.sql
+++ import/sql/tile_views.sql
@@ -356,8 +356,8 @@ CREATE OR REPLACE VIEW railway_text_stations AS
     state,
     station,
     CASE
-      WHEN route_count >= 20 THEN 'large'
-      WHEN route_count >= 8 THEN 'normal'
+      WHEN importance >= 20 THEN 'large'
+      WHEN importance >= 8 THEN 'normal'
       ELSE 'small'
     END AS station_size,
     name,
@@ -378,7 +378,7 @@ CREATE OR REPLACE VIEW railway_text_stations AS
       ELSE 50
     END AS rank,
     uic_ref,
-    route_count,
+    importance,
     count,
     nullif(array_to_string(operator, U&'\001E'), '') as operator,
     nullif(array_to_string(network, U&'\001E'), '') as network,
@@ -394,10 +394,10 @@ CREATE OR REPLACE VIEW railway_text_stations AS
     nullif(array_to_string(description, U&'\001E'), '') as description,
     nullif(array_to_string(yard_purpose, U&'\001E'), '') as yard_purpose,
     yard_hump
-  FROM grouped_stations_with_route_count
+  FROM grouped_stations_with_importance
   ORDER BY
     rank DESC NULLS LAST,
-    route_count DESC NULLS LAST;
+    importance DESC NULLS LAST;
 
 CREATE OR REPLACE FUNCTION standard_railway_text_stations_low(z integer, x integer, y integer)
   RETURNS bytea
@@ -440,9 +440,9 @@ RETURN (
       AND state = 'present'
       AND (station IS NULL OR station NOT IN ('light_rail', 'monorail', 'subway'))
       AND railway_ref IS NOT NULL
-      AND route_count >= 8
+      AND importance >= 8
     ORDER BY
-      route_count DESC NULLS LAST
+      importance DESC NULLS LAST
   ) as tile
   WHERE way IS NOT NULL
 );
@@ -525,7 +525,7 @@ RETURN (
       AND state = 'present'
       AND (station IS NULL OR station NOT IN ('light_rail', 'monorail', 'subway'))
       AND railway_ref IS NOT NULL
-      AND route_count >= 20
+      AND importance >= 20
   ) as tile
   WHERE way IS NOT NULL
 );
@@ -783,7 +783,7 @@ RETURN (
       nullif(array_to_string(wikipedia, U&'\001E'), '') as wikipedia,
       nullif(array_to_string(note, U&'\001E'), '') as note,
       nullif(array_to_string(description, U&'\001E'), '') as description
-    FROM grouped_stations_with_route_count
+    FROM grouped_stations_with_importance
     WHERE buffered && ST_TileEnvelope(z, x, y)
   ) as tile
   WHERE way IS NOT NULL
