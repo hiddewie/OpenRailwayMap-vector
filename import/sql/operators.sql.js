94bc63fc0a66da1b0665c601cc48ce12a6614341
Hidde Wieringa <hidde@hiddewieringa.nl>
2025-12-06 10:54:56 +0100

    operators view

---
+++ import/sql/operators.sql.js
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import yaml from 'yaml'
+
+const operators = yaml.parse(fs.readFileSync('operator.yaml', 'utf8'))
+
+const operatorsByName = operators.operators
+  .flatMap(({names, color}) => names.map(name => ({name, color})));
+
+/**
+ * Template that builds the SQL view taking the YAML configuration into account
+ */
+const sql = `
+CREATE OR REPLACE VIEW railway_operator_view AS
+  SELECT
+    row_number() as id,
+    name,
+    color
+  FROM (VALUES
+    ${operatorsByName.map(({name, color}) => `
+    ('${name}', '${color}')`).join(',')}
+  ) v (name, color);
+
+-- Use the view directly such that the query in the view can be updated
+CREATE MATERIALIZED VIEW IF NOT EXISTS railway_operator AS
+  SELECT
+    *
+  FROM
+    railway_operator_view;
+
+CREATE INDEX IF NOT EXISTS railway_operator_name
+  ON railway_operator
+    USING btree(name);
+`
+
+console.log(sql);
