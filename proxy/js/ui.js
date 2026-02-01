103364957f6ae9e53fe542c929c4b8e397ee26db
Hidde Wieringa <hidde@hiddewieringa.nl>
2026-02-01 21:14:40 +0100

    Fix missing default configuration for gauge in track layer (#797)
    Fixes #794
    
    If the user has not configured the preferred track layer visualization,
    the track layer will default to the gauge. The configuration UI also
    shows this default. However the map did not incorporate it in all
    places, showing every line as having an unknown gauge.

--- proxy/js/ui.js
+++ proxy/js/ui.js
@@ -1142,7 +1142,8 @@ const defaultConfiguration = {
   view: {},
   stationLowZoomLabel: 'label',
   localization: 'automatic',
-  electrificationRailwayLine: 'voltageFrequency'
+  electrificationRailwayLine: 'voltageFrequency',
+  trackRailwayLine: 'gauge',
 };
 let configuration = readConfiguration(localStorage);
 configuration = migrateConfiguration(localStorage, configuration);
