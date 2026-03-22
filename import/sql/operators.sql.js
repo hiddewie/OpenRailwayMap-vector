26da837355cb7944a78afa941b256e913ab8393d
Hidde Wieringa <hidde@hiddewieringa.nl>
2026-03-22 16:50:47 +0100

    import if colors are bright

--- import/sql/operators.sql.js
+++ import/sql/operators.sql.js
@@ -6,6 +6,15 @@ const operators = yaml.parse(fs.readFileSync('operators.yaml', 'utf8'))
 const operatorsByName = operators.operators
   .flatMap(({names, color}) => names.map(name => ({name, color})));
 
+const rgb = /^#[0-9a-fA-F]{3}/
+const rrggbb = /^#[0-9a-fA-F]{6}/
+const rgbf = /^rgb\(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]), *([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]), *([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\)/
+const hslf = /^hsl\(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]), *([0-9]|[1-9][0-9]|100)%, *([0-9]|[1-9][0-9]|100)%\)/
+
+function brightColor(color) {
+  return true;
+}
+
 /**
  * Template that builds the SQL view taking the YAML configuration into account
  */
@@ -14,9 +23,10 @@ CREATE OR REPLACE VIEW railway_operator_view AS
   SELECT
     row_number() over () as id,
     name,
-    color
+    color,
+    bright
   FROM (VALUES${operatorsByName.map(({name, color}) => `
-    ('${name}', '${color}')`).join(',')}
+    ('${name}', '${color}', ${brightColor(color)})`).join(',')}
   ) operator_data (name, color);
 
 -- Use the view directly such that the query in the view can be updated
