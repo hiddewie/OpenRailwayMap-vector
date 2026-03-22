34000e9aae40d2c467fde363d179147a6d4e0385
Hidde Wieringa <hidde@hiddewieringa.nl>
2026-03-22 20:06:14 +0100

    hsl luminance

--- import/sql/operators.sql.js
+++ import/sql/operators.sql.js
@@ -13,7 +13,7 @@ const rrggbb = /^#[0-9a-fA-F]{6}$/
 // rgb(123, 0,255)
 const rgbf = /^rgb\(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]), *([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]), *([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\)$/
 // hsl(123, 0%, 34%)
-const hslf = /^hsl\(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]), *([0-9]|[1-9][0-9]|100)%, *([0-9]|[1-9][0-9]|100)%\)$/
+const hslf = /^hsl\(([0-9]|[1-9][0-9]|[12][0-9]{2}|3[0-5][0-9]), *([0-9]|[1-9][0-9]|100)%, *([0-9]|[1-9][0-9]|100)%\)$/
 
 // See https://www.w3.org/WAI/GL/wiki/Relative_luminance and https://beta.stackoverflow.com/q/3942878#3943023
 function rgbLuminance(r, g, b) {
@@ -55,9 +55,49 @@ function brightColor(name, color) {
     return isBright(rgbLuminance(red, green, blue))
   } else if (color.match(hslf)) {
     const matches = color.match(hslf);
-    const luminance = parseInt(matches[3])
+    const hue = parseInt(matches[1])
+    const saturation = parseInt(matches[2])
+    const lightness = parseInt(matches[3])
 
-    return isBright(luminance)
+
+    /**
+     * Converts an HSL color value to RGB. Conversion formula
+     * adapted from https://en.wikipedia.org/wiki/HSL_color_space.
+     * Assumes h, s, and l are contained in the set [0, 1] and
+     * returns r, g, and b in the set [0, 255].
+     *
+     * @param   {number}  h       The hue
+     * @param   {number}  s       The saturation
+     * @param   {number}  l       The lightness
+     * @return  {Array}           The RGB representation
+     */
+    function hslToRgb(h, s, l) {
+      let r, g, b;
+
+      if (s === 0) {
+        r = g = b = l; // achromatic
+      } else {
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hueToRgb(p, q, h + 1/3);
+        g = hueToRgb(p, q, h);
+        b = hueToRgb(p, q, h - 1/3);
+      }
+
+      return [r * 255,g * 255 , b * 255];
+    }
+
+    function hueToRgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    const [red, green, blue] = hslToRgb(hue / 360.0, saturation/ 100.0, lightness/ 100.0)
+    return isBright(rgbLuminance(red, green, blue))
   } else {
     throw new Error(`Could not match color '${color}' against known color patterns`)
   }
