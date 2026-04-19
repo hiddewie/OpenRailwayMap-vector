084ed978159b3b869aec8dcc58ca45b3cc5d9610
Hidde Wieringa <hidde@hiddewieringa.nl>
2026-04-19 17:51:48 +0200

    simplify querying by ref

--- api/openrailwaymap_api/facility_api.py
+++ api/openrailwaymap_api/facility_api.py
@@ -62,14 +62,11 @@ class FacilityAPI:
         """
         return await self.query_result(sql_query, (q, language, limit))
 
-    async def _search_by_ref(self, search_function, ref, language, limit):
+    async def search_by_ref(self, ref, language, limit):
         sql_query = f"""
-          SELECT * FROM {search_function}($1, $2, $3)
+          SELECT * FROM query_facilities_by_ref($1, $2, $3)
         """
         return await self.query_result(sql_query, (ref, language, limit))
-
-    async def search_by_ref(self, ref, language, limit):
-        return await self._search_by_ref("query_facilities_by_ref", ref, language, limit)
 
     async def query_result(self, sql_query, parameters):
         async with self.database.acquire() as connection:
