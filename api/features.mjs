7374132c160e4815def28e3d941bea792e444546
Hidde Wieringa <hidde@hiddewieringa.nl>
2026-05-03 20:11:17 +0200

    Migrate switch popup from map tiles to API (#932)
    For #796
    
    
    <img width="850" height="406" alt="image"
    src="https://github.com/user-attachments/assets/471b7e9b-3ad2-4894-95aa-db955c53b681"
    />
    
    <img width="850" height="406" alt="image"
    src="https://github.com/user-attachments/assets/620a69cd-6647-42c0-b627-d269b8f04874"
    />

--- api/features.mjs
+++ api/features.mjs
@@ -808,6 +808,11 @@ const features = {
     },
   },
   'openrailwaymap_standard-standard_railway_switch_ref': {
+    view: {
+      name: 'standard_railway_switch_view',
+      id_type: 'numeric',
+    },
+    labelProperties: ['ref'],
     featureProperty: 'railway',
     featureLinks: featureLinks.openstreetmap,
     features: {
@@ -819,9 +824,6 @@ const features = {
       }
     },
     properties: {
-      ref: {
-        name: 'Reference',
-      },
       type: {
         name: 'Type',
       },
