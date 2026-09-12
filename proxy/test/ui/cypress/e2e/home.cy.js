b19e7d1bc26e32c335e894752f8915f0f8ff6da6
Hidde Wieringa <hidde@hiddewieringa.nl>
2026-09-12 17:23:46 +0200

    UI tests, switch date back, assert tracks URL parameters

--- proxy/test/ui/cypress/e2e/home.cy.js
+++ proxy/test/ui/cypress/e2e/home.cy.js
@@ -9,7 +9,7 @@ describe('home page', () => {
     cy.contains('.maplibregl-ctrl-preset button', 'Presets').click()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Infrastructure').click()
-    cy.url().should('not.include', 'style=standard')
+    cy.url().should('not.include', 'tracks=usage')
 
     cy.wait(3000)
     cy.screenshot()
@@ -21,32 +21,42 @@ describe('home page', () => {
     cy.wait(3000)
     cy.screenshot()
 
+    cy.get('.maplibregl-ctrl-date input[type=range]').invoke('val', (new Date()).getFullYear()).trigger('input').trigger('change')
+    cy.get('.date-display').should('have.value', 'present')
+    cy.url().should('not.include', 'date=')
+
     cy.contains('.maplibregl-ctrl-preset button', 'Speed').click()
-    cy.url().should('include', 'style=speed')
+    cy.url().should('include', 'tracks=speed')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Train protection').click()
-    cy.url().should('include', 'style=signals')
+    cy.url().should('include', 'tracks=train_protection')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Electrification').click()
-    cy.url().should('include', 'style=electrification')
+    cy.url().should('include', 'tracks=electrification')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Track').click()
-    cy.url().should('include', 'style=track')
+    cy.url().should('include', 'tracks=track')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Operator').click()
-    cy.url().should('include', 'style=operator')
+    cy.url().should('include', 'tracks=operator')
+
+    cy.wait(3000)
+    cy.screenshot()
+
+    cy.contains('.maplibregl-ctrl-preset button', 'Routes').click()
+    cy.url().should('include', 'tracks=routes')
 
     cy.wait(3000)
     cy.screenshot()
@@ -63,7 +73,7 @@ describe('home page', () => {
 
     cy.get('#configuration-backdrop .btn-close').click()
     cy.contains('Map configuration').should('not.be.visible')
-    cy.url().should('not.include', 'style=standard')
+    cy.url().should('not.include', 'tracks=usage')
 
     cy.wait(3000)
     cy.screenshot()
@@ -75,34 +85,44 @@ describe('home page', () => {
     cy.wait(3000)
     cy.screenshot()
 
+    cy.get('.maplibregl-ctrl-date input[type=range]').invoke('val', (new Date()).getFullYear()).trigger('input').trigger('change')
+    cy.get('.date-display').should('have.value', 'present')
+    cy.url().should('not.include', 'date=')
+
     cy.contains('.maplibregl-ctrl-preset button', 'Presets').click()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Speed').click()
-    cy.url().should('include', 'style=speed')
+    cy.url().should('include', 'tracks=speed')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Train protection').click()
-    cy.url().should('include', 'style=signals')
+    cy.url().should('include', 'tracks=train_protection')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Electrification').click()
-    cy.url().should('include', 'style=electrification')
+    cy.url().should('include', 'tracks=electrification')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Track').click()
-    cy.url().should('include', 'style=track')
+    cy.url().should('include', 'tracks=track')
 
     cy.wait(3000)
     cy.screenshot()
 
     cy.contains('.maplibregl-ctrl-preset button', 'Operator').click()
-    cy.url().should('include', 'style=operator')
+    cy.url().should('include', 'tracks=operator')
+
+    cy.wait(3000)
+    cy.screenshot()
+
+    cy.contains('.maplibregl-ctrl-preset button', 'Routes').click()
+    cy.url().should('include', 'tracks=routes')
 
     cy.wait(3000)
     cy.screenshot()
